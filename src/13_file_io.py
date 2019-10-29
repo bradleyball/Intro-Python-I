52087f8b84dd9d14d4ee25d575e35c0fbc6fca96
cacheUp <bradb26@gmail.com>
2019-10-29 14:36:59 +0300

    file io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+# with open('/Users/mr.ball/Documents/coding/lambda/python/python-intro/Intro-Python-I/src/foo.txt') as f:
+#     read_data = f.read()
+#     print(read_data)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+c = open('bar.txt', 'w+')
+for i in range(3):
+    c.write("Added MORE TEXT WOOOOOAAAH\n ")
+c.close()
+r = open('/Users/mr.ball/Documents/coding/lambda/python/python-intro/Intro-Python-I/bar.txt', 'r')
+print(r.read())
+
 
 # YOUR CODE HERE
